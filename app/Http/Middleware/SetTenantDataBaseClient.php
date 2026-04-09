964d1783efeada909b340a39eec9656fe65bcfff
Rodrigo Teixeira Oliveira <rodrigoteixeira.oli0@gmail.com>
2026-04-09 12:22:43 -0300

    update slug/subdomain verification

--- app/Http/Middleware/SetTenantDataBaseClient.php
+++ app/Http/Middleware/SetTenantDataBaseClient.php
@@ -18,12 +18,41 @@ class SetTenantDataBaseClient
     public function handle(Request $request, Closure $next): Response
     {
         try {
-            $slug = explode('.', $request->getHost())[0];
-            $store = Store::where('slug', $slug)->firstOrFail();
+            if(app()->environment('production')){
+                $host = $request->getHost();
+                $parts = explode('.', $host);
+                if (count($parts) < 3) {
+                    throw new \Exception('Subdomínio não informado');
+                }
+                if ($parts[0] === 'www') {
+                    throw new \Exception('Erro de subdomínio, por favor acesse sem o www');
+                }
+                $slug = $parts[0];
+                $store = Store::where('slug', $slug)->first();
+                if(!$store){
+                    throw new \Exception('Empresa não encontrada');
+                }
+            }elseif(app()->environment('local')){
+                $host = $request->getHost();
+                $parts = explode('.', $host);
+                if (count($parts) < 2) {
+                    throw new \Exception('Subdomínio não informado');
+                }
+                if ($parts[0] === 'www') {
+                    throw new \Exception('Erro de subdomínio, por favor acesse sem o www');
+                }
+                $slug = $parts[0];
+                $store = Store::where('slug', $slug)->first();
+                if(!$store){
+                    throw new \Exception('Empresa não encontrada');
+                }
+            }else{
+                throw new \Exception('Ambiente não configurado para multi-tenant');
+            }
             app()->instance('slug', $slug);
             return $next($request);
         } catch (\Throwable $th) {
-            return abort(403,'Empresa não encontrada');
+            return abort(403,$th->getMessage());
         }
     }
 }
